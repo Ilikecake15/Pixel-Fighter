3115aef19d7fd36dcb23b9f79f74323cf770ca59
Charlie <117092824+Ilikecake15@users.noreply.github.com>
2026-02-20 06:05:12 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,255 +1,137 @@
-alert("NEW VERSION LOADED");
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 1000;
 canvas.height = 500;
 
-let gameState = "select";
-let round = 1;
-let maxRounds = 3;
-let screenShake = 0;
+let gameState = "title";
+let menuIndex = 0;
+let charIndex = 0;
 
-const gravity = 0.8;
-const ground = 420;
+const menuOptions = [
+  "Start Game",
+  "Options",
+  "Credits"
+];
 
-let sounds = {
-  hit: new Audio("https://assets.mixkit.co/sfx/preview/mixkit-arcade-game-jump-coin-216.wav"),
-  special: new Audio("https://assets.mixkit.co/sfx/preview/mixkit-arcade-retro-changing-tab-206.wav")
-};
+const characters = [
+  {name:"Blaze", color:"red", speed:6, jump:15, knockback:8},
+  {name:"Volt", color:"cyan", speed:4, jump:13, knockback:15}
+];
 
-class Particle {
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-    this.life = 20;
+let keys = {};
+
+document.addEventListener("keydown", e=>{
+  if(!keys[e.key]){
+    handleInput(e.key);
   }
-  draw(){
-    ctx.fillStyle="orange";
-    ctx.fillRect(this.x,this.y,4,4);
-  }
-  update(){
-    this.y -=2;
-    this.life--;
-  }
-}
-
-class Projectile {
-  constructor(x,y,dir){
-    this.x=x;
-    this.y=y;
-    this.dir=dir;
-    this.speed=10;
-    this.size=15;
-  }
-  update(){
-    this.x+=this.speed*this.dir;
-  }
-  draw(){
-    ctx.fillStyle="yellow";
-    ctx.fillRect(this.x,this.y,this.size,this.size);
-  }
-}
-
-class Fighter{
-  constructor(x,color,stats){
-    this.x=x;
-    this.y=ground;
-    this.width=50;
-    this.height=90;
-    this.color=color;
-
-    this.health=100;
-    this.velX=0;
-    this.velY=0;
-
-    this.speed=stats.speed;
-    this.jumpPower=stats.jump;
-    this.knockback=stats.knockback;
-
-    this.onGround=true;
-    this.projectiles=[];
-  }
-
-  draw(){
-    ctx.fillStyle=this.color;
-    ctx.fillRect(this.x,this.y-this.height,this.width,this.height);
-  }
-
-  update(){
-    this.velY+=gravity;
-    this.y+=this.velY;
-    this.x+=this.velX;
-
-    if(this.y>ground){
-      this.y=ground;
-      this.velY=0;
-      this.onGround=true;
-    }
-
-    if(this.x<0) this.x=0;
-    if(this.x>canvas.width-this.width)
-      this.x=canvas.width-this.width;
-
-    this.projectiles.forEach(p=>p.update());
-  }
-
-  jump(){
-    if(this.onGround){
-      this.velY=-this.jumpPower;
-      this.onGround=false;
-    }
-  }
-
-  attack(opponent){
-    if(Math.abs(this.x-opponent.x)<70){
-      opponent.health-=8;
-      opponent.velX=(opponent.x>this.x?1:-1)*this.knockback;
-      screenShake=10;
-      sounds.hit.play();
-      createParticles(opponent.x,opponent.y);
-    }
-  }
-
-  special(opponent){
-    let dir = opponent.x>this.x?1:-1;
-    this.projectiles.push(new Projectile(this.x+25,this.y-50,dir));
-    sounds.special.play();
-  }
-}
-
-const characters={
-  Blaze:{color:"red",speed:6,jump:15,knockback:8},
-  Volt:{color:"cyan",speed:4,jump:13,knockback:15}
-};
-
-let player1,player2;
-let particles=[];
-let keys={};
-
-document.addEventListener("keydown",e=>{
   keys[e.key]=true;
-  if(gameState==="select"){
-    if(e.key==="1") startGame("Blaze");
-    if(e.key==="2") startGame("Volt");
-  }
 });
-document.addEventListener("keyup",e=>keys[e.key]=false);
+document.addEventListener("keyup", e=> keys[e.key]=false);
 
-function startGame(name){
-  player1=new Fighter(150,characters[name].color,characters[name]);
-  player2=new Fighter(800,"blue",characters["Volt"]);
+function handleInput(key){
+
+  if(gameState==="title"){
+    gameState="mainMenu";
+  }
+
+  else if(gameState==="mainMenu"){
+    if(key==="ArrowUp") menuIndex--;
+    if(key==="ArrowDown") menuIndex++;
+    if(key==="Enter"){
+      if(menuOptions[menuIndex]==="Start Game"){
+        gameState="characterSelect";
+      }
+    }
+
+    if(menuIndex<0) menuIndex=menuOptions.length-1;
+    if(menuIndex>=menuOptions.length) menuIndex=0;
+  }
+
+  else if(gameState==="characterSelect"){
+    if(key==="ArrowLeft") charIndex--;
+    if(key==="ArrowRight") charIndex++;
+    if(key==="Enter"){
+      startGame();
+    }
+
+    if(charIndex<0) charIndex=characters.length-1;
+    if(charIndex>=characters.length) charIndex=0;
+  }
+
+  else if(gameState==="gameOver"){
+    if(key==="Enter"){
+      gameState="mainMenu";
+    }
+  }
+}
+
+function drawTitle(){
+  ctx.fillStyle="white";
+  ctx.font="60px monospace";
+  ctx.fillText("PIXEL FIGHTERS",250,200);
+
+  ctx.font="20px monospace";
+  ctx.fillText("Press Any Key",400,300);
+}
+
+function drawMainMenu(){
+  ctx.fillStyle="white";
+  ctx.font="40px monospace";
+  ctx.fillText("MAIN MENU",350,120);
+
+  ctx.font="28px monospace";
+
+  menuOptions.forEach((option,i)=>{
+    if(i===menuIndex){
+      ctx.fillStyle="yellow";
+    } else {
+      ctx.fillStyle="white";
+    }
+    ctx.fillText(option,420,200+i*50);
+  });
+}
+
+function drawCharacterSelect(){
+  ctx.fillStyle="white";
+  ctx.font="40px monospace";
+  ctx.fillText("SELECT YOUR FIGHTER",250,100);
+
+  characters.forEach((char,i)=>{
+    let x = 300 + i*300;
+
+    ctx.fillStyle = i===charIndex ? "yellow" : "white";
+    ctx.fillText(char.name,x,200);
+
+    ctx.fillStyle = char.color;
+    ctx.fillRect(x+30,250,100,150);
+
+    ctx.fillStyle="white";
+    ctx.font="16px monospace";
+    ctx.fillText("Speed: "+char.speed,x,420);
+    ctx.fillText("Jump: "+char.jump,x,440);
+    ctx.fillText("Power: "+char.knockback,x,460);
+  });
+}
+
+function startGame(){
   gameState="fight";
 }
 
-function aiLogic(){
-  if(player2.x>player1.x) player2.velX=-2;
-  else player2.velX=2;
-
-  if(Math.random()<0.02) player2.attack(player1);
-  if(Math.random()<0.01) player2.special(player1);
-}
-
-function createParticles(x,y){
-  for(let i=0;i<10;i++){
-    particles.push(new Particle(x+Math.random()*30,y-50));
-  }
-}
-
-function checkProjectiles(){
-  player1.projectiles.forEach(p=>{
-    if(Math.abs(p.x-player2.x)<40){
-      player2.health-=15;
-      screenShake=15;
-    }
-  });
-
-  player2.projectiles.forEach(p=>{
-    if(Math.abs(p.x-player1.x)<40){
-      player1.health-=15;
-      screenShake=15;
-    }
-  });
-}
-
-function drawHealth(){
-  ctx.fillStyle="green";
-  ctx.fillRect(20,20,player1.health*3,20);
-  ctx.fillRect(650,20,player2.health*3,20);
-}
-
-function drawSelect(){
+function drawFightPlaceholder(){
   ctx.fillStyle="white";
   ctx.font="30px monospace";
-  ctx.fillText("Press 1 - Blaze (Fast Fire)",300,200);
-  ctx.fillText("Press 2 - Volt (Heavy Knockback)",300,260);
-}
-
-function checkWinner(){
-  if(player1.health<=0 || player2.health<=0){
-    gameState="roundOver";
-    setTimeout(()=>{
-      if(round<maxRounds){
-        round++;
-        startGame("Blaze");
-      }else{
-        gameState="gameOver";
-      }
-    },2000);
-  }
+  ctx.fillText("FIGHT STARTING...",350,250);
 }
 
 function update(){
-  ctx.save();
-
-  if(screenShake>0){
-    ctx.translate(Math.random()*10-5,Math.random()*10-5);
-    screenShake--;
-  }
-
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
-  if(gameState==="select"){
-    drawSelect();
-    requestAnimationFrame(update);
-    return;
-  }
+  if(gameState==="title") drawTitle();
+  else if(gameState==="mainMenu") drawMainMenu();
+  else if(gameState==="characterSelect") drawCharacterSelect();
+  else if(gameState==="fight") drawFightPlaceholder();
 
-  if(gameState==="gameOver"){
-    ctx.fillStyle="white";
-    ctx.font="40px monospace";
-    ctx.fillText("GAME OVER",400,250);
-    return;
-  }
-
-  player1.velX=0;
-
-  if(keys["a"]) player1.velX=-player1.speed;
-  if(keys["d"]) player1.velX=player1.speed;
-  if(keys[" "]) player1.jump();
-  if(keys["w"]) player1.attack(player2);
-  if(keys["s"]) player1.special(player2);
-
-  aiLogic();
-
-  player1.update();
-  player2.update();
-
-  checkProjectiles();
-  checkWinner();
-
-  player1.draw();
-  player2.draw();
-
-  player1.projectiles.forEach(p=>p.draw());
-  player2.projectiles.forEach(p=>p.draw());
-
-  particles.forEach(p=>{p.update();p.draw();});
-  particles=particles.filter(p=>p.life>0);
-
-  drawHealth();
-
-  ctx.restore();
   requestAnimationFrame(update);
 }
 

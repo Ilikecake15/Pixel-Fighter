f02b8493f6d365fe13537eda5770eccc574e2dd8
Charlie <117092824+Ilikecake15@users.noreply.github.com>
2026-02-20 15:20:06 -0500

    Update game.js

--- game.js
+++ game.js
@@ -26,8 +26,8 @@ function drawBackground() {
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1000;
-canvas.height = 500;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 ctx.imageSmoothingEnabled = false;
 ctx.textAlign = "center";
